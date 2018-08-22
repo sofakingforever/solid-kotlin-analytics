6ac81a5820a768286e275be56e48d80f92d35ef2
sofakingforever <sofakingforever@gmail.com>
2018-08-22 16:49:51 +0300

    Version 1.0.6

--- app/src/main/java/com/sofakingforever/example/App.kt
+++ app/src/main/java/com/sofakingforever/example/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import com.sofakingforever.analytics.Analytics
 import com.sofakingforever.analytics.AnalyticsSettings
+import com.sofakingforever.analytics.kits.answers.AnswersDispatcherImpl
 import com.sofakingforever.example.custom.CustomDispatcher
 import com.sofakingforever.example.custom.LoggerDispatcherImpl
 
