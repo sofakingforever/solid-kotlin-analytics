af228eee63100242a8f8e030353e029c539d9e7c
sofakingforever <sofakingforever@gmail.com>
2018-08-27 14:43:17 +0300

    Extracted VersionChecker

--- analytics/src/main/java/com/sofakingforever/analytics/version/VersionChecker.kt
+++ analytics/src/main/java/com/sofakingforever/analytics/version/VersionChecker.kt
@@ -1,24 +1,18 @@
 package com.sofakingforever.analytics.version
 
-import android.util.Log
 import com.sofakingforever.library.BuildConfig
+import com.sofakingforever.repoverse.Version
+import com.sofakingforever.repoverse.resolvers.BintrayVersionResolver
 
 object VersionChecker {
 
-    val currentVersion = Version(BuildConfig.VERSION_NAME)
+    private val currentVersion = Version(BuildConfig.VERSION_NAME)
 
-    private val callback: RemoteVersionResolver.Callback = object : RemoteVersionResolver.Callback {
-        override fun onVersionResolved(latestVersion: Version) {
-            if (currentVersion < latestVersion) {
-                // user should update
-                Log.w("kotlin-analytics", "Latest library version $latestVersion > $currentVersion (current)")
-            }
-        }
-
-    }
+    private const val packagePath: String = "sofakingforever/analytics/kotlin-analytics"
 
     fun invoke() {
-        BintrayVersionResolver("sofakingforever/analytics/kotlin-analytics").resolve(callback)
+        BintrayVersionResolver(packagePath)
+                .resolve(VersionResolverCallback(currentVersion))
 
     }
 
